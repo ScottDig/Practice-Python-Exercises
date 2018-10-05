14f5f76de56806ab774505c7c424ea554d78e0d3
ScottDig <sdigiam@gmail.com>
2018-10-04 21:32:01 -0700

    push

--- ex14.py
+++ ex14.py
@@ -2,7 +2,7 @@
 
 
 
-def removeListDups(lst):
+def removeListDups1(lst):
     newlst = []
     while bool(len(lst)):
         x = lst.pop()
@@ -11,6 +11,24 @@ def removeListDups(lst):
     newlst.sort()
     return newlst
 
-a = [1, 2, 3, 4, 4, 4, 1, 2, 5, 6, 2]
 
-print(removeListDups(a))
+def removeListDups2(lst):
+    newlst = []
+    for x in lst:
+        if x not in newlst:
+            newlst.append(x)
+    newlst.sort()
+    return newlst
+
+
+def removeListDups3(lst):
+    newlst = list(set(lst))
+    return newlst
+
+
+a = [1, 2, 3, 4, 4, 4, 1, 2, 5, 6, 2]
+print(removeListDups1(a))
+a = [1, 2, 3, 4, 4, 4, 1, 2, 5, 6, 2]
+print(removeListDups2(a))
+a = [1, 2, 3, 4, 4, 4, 1, 2, 5, 6, 2]
+print(removeListDups3(a))
